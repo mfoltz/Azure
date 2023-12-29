8a98869f50b6a45e0d11015466307e7061268e8d
mfoltz <mitchell.foltz@gmail.com>
2023-12-29 12:52:36 -0600

    adding some commands for resetlevel

--- RPGAddOns/Commands.cs
+++ RPGAddOns/Commands.cs
@@ -2,6 +2,7 @@
 using System.Text.Json;
 using Unity.Entities;
 using VampireCommandFramework;
+using RPGMods;
 
 namespace RPGAddOns
 {
@@ -42,7 +43,58 @@ namespace RPGAddOns
 
 
         }
+        [Command(name: "getresets", shortHand: "gr", adminOnly: false, usage: "Check your current reset count.", description: "Displays the number of times you have reset your level.")]
+        public static void CheckResetsCommand(ChatCommandContext ctx)
+        {
+            var user = ctx.Event.User;
+            ulong SteamID = user.PlatformId;
 
+            if (Databases.playerResetCountsBuffs.TryGetValue(SteamID, out ResetData data))
+            {
+                ctx.Reply($"Your current reset count is: {data.ResetCount}");
+            }
+            else
+            {
+                ctx.Reply("You have not reset your level yet.");
+            }
+        }
+        [Command(name: "getbuffs", shortHand: "gb", adminOnly: false, usage: "Check your current permanent buffs.", description: "Displays the buffs you have received from resets.")]
+        public static void CheckBuffsCommand(ChatCommandContext ctx)
+        {
+            var user = ctx.Event.User;
+            ulong SteamID = user.PlatformId;
+
+            if (Databases.playerResetCountsBuffs.TryGetValue(SteamID, out ResetData data))
+            {
+                var buffs = data.Buffs.Count > 0 ? string.Join(", ", data.Buffs) : "None";
+                ctx.Reply($"Your current buffs are: {buffs}");
+            }
+            else
+            {
+                ctx.Reply("You have not received any buffs yet.");
+            }
+        }
+        [Command(name: "wiperesets", shortHand: "wr", adminOnly: true, usage: "Wipe a user's progress.", description: "Resets the specified user's reset count and buffs to the initial state. Does not wipe any buffs they already have but that would probably be good to add here")]
+        public static void WipeProgressCommand(ChatCommandContext ctx, string playerName)
+        {
+            // Find the user's SteamID based on the playerName
+            
+            
+            RPGMods.Utils.Helper.FindPlayer(playerName, false, out Entity playerEntity, out Entity userEntity);
+            ulong SteamID = (ulong)VWorld.Server.EntityManager.GetComponentData<PlatformID>(playerEntity);
+            if (Databases.playerResetCountsBuffs.ContainsKey(SteamID))
+            {
+                // Reset the user's progress
+                Databases.playerResetCountsBuffs[SteamID] = new ResetData(0, []);
+                Commands.SavePlayerResets();  // Assuming this method saves the data to a persistent storage
+
+                ctx.Reply($"Progress for player {playerName} has been wiped.");
+            }
+            else
+            {
+                ctx.Reply($"Player {playerName} not found or no progress to wipe.");
+            }
+        }
         private static EntityManager entityManager = VWorld.Server.EntityManager;
         public static void LoadData()
         {
