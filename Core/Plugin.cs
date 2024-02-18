8812fa1ad4c400ebbab107580f3c428acef7400c
mfoltz <mitchell.foltz@gmail.com>
2024-02-17 18:24:58 -0600

    missed line for shard drop config, added

--- Core/Plugin.cs
+++ Core/Plugin.cs
@@ -100,8 +100,8 @@ namespace RPGAddOnsEx.Core
 
             BuffRewardsPrestige = Config.Bind("Config", "BuffRewardsReset", true, "Grants permanent buff to players when resetting if enabled.").Value;
             BuffRewardsRankUp = Config.Bind("Config", "BuffRewardsPrestige", true, "Grants permanent buff to players when prestiging if enabled.").Value;
-            BuffPrefabsPrestige = Config.Bind("Config", "BuffPrefabsPrestige", "[1425734039,-91451769,1491093272,1163490655,-1572696947,-1559874083,-1124645803]", "Buff prefabs to give players when resetting. Granted in order, want # buffs == # levels [Buff1, Buff2, etc] to skip buff for a level set it to be 0").Value;
-            BuffPrefabsRankUp = Config.Bind("Config", "BuffPrefabsRank", "[-1100642493,1637213963,476186894,1195333673,-2008977590,546136204,-2054010438,1883675026,-2064810948]", "Buff prefabs to give players when prestiging. Granted in order, want # buffs == # prestige [Buff1, Buff2, etc] if enabled to skip buff for a level set it to be 0").Value;
+            BuffPrefabsPrestige = Config.Bind("Config", "BuffPrefabsPrestige", "[0,0,0,0,0]", "Buff prefabs to give players when resetting. Granted in order, want # buffs == # levels [Buff1, Buff2, etc] to skip buff for a level set it to be 0").Value;
+            BuffPrefabsRankUp = Config.Bind("Config", "BuffPrefabsRank", "[0,0,0,0,0]", "Buff prefabs to give players when prestiging. Granted in order, want # buffs == # prestige [Buff1, Buff2, etc] if enabled to skip buff for a level set it to be 0").Value;
 
             modifyDeathSetStats = Config.Bind("Config", "ModifyDeathSetStats", true, "Modify the stats of the death set").Value;
             modifyDeathSetBonus = Config.Bind("Config", "ModifyDeathSetBonus", true, "Modify the set bonus of the death set").Value;
@@ -109,6 +109,7 @@ namespace RPGAddOnsEx.Core
             extraStatType = Config.Bind("Config", "ExtraStatType", "SpellPower", "Stat type to add to the death set").Value;
             extraStatValue = Config.Bind("Config", "ExtraStatValue", 5, "Stat value to add to the death set").Value;
 
+            shardDrop = Config.Bind("Config", "ShardDrop", true, "Enable shard drop from Solarus").Value;
             if (!Directory.Exists(ConfigPath))
             {
                 Directory.CreateDirectory(ConfigPath);
