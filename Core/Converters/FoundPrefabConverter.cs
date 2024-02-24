96a6aeb640e778fd6d758ab947e1bc0229d431ba
mfoltz <mitchell.foltz@gmail.com>
2024-02-24 03:23:56 -0600

    sync

---
+++ Core/Converters/FoundPrefabConverter.cs
@@ -0,0 +1,23 @@
+﻿using ProjectM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using V.Core.Tools;
+using V.Data;
+using VampireCommandFramework;
+
+namespace V.Core.Converters
+{
+    internal class FoundPrefabConverter : CommandArgumentConverter<FoundPrefabGuid>
+    {
+        public override FoundPrefabGuid Parse(ICommandContext ctx, string input)
+        {
+            PrefabGUID prefabGUID;
+            if (Helper.TryGetPrefabGUIDFromString(input, out prefabGUID))
+                return new FoundPrefabGuid(prefabGUID);
+            throw ctx.Error("Could not find buff: " + input);
+        }
+    }
+}
