5de41ffe5d17c7ef9c5a4b83cb98992654fdfcff
mfoltz <mitchell.foltz@gmail.com>
2024-03-22 17:39:28 -0500

    sync

--- Hooks/PersistenceSaveSystem.cs
+++ Hooks/PersistenceSaveSystem.cs
@@ -71,7 +71,6 @@ namespace VPlus.Hooks
         private static bool isRunning = false;
         private static Entity infinite = Entity.Null;
         private static Entity zone = Entity.Null;
-        private static Entity storm = Entity.Null;
         private static int otherTimer = 0;
 
         public static void RunMethods()
@@ -80,7 +79,7 @@ namespace VPlus.Hooks
             timer += 2; // want to run event every 2 hours and save happens every 2 minutes
             EntityCommandBufferSystem entityCommandBufferSystem = VWorld.Server.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
             EntityCommandBuffer ecb = entityCommandBufferSystem.CreateCommandBuffer();
-            if (timer > 2)
+            if (timer > 4)
             {
                 timer = 0;
                 isRunning = true;
@@ -90,25 +89,15 @@ namespace VPlus.Hooks
                 {
                     EntityManager entityManager = VWorld.Server.EntityManager;
                     float3 center = new(-1001, 0, -514);
-                    Entity nodePrefab = VWorld.Server.GetExistingSystem<PrefabCollectionSystem>()._PrefabGuidToEntityMap[VCreate.Data.Prefabs.TM_Crystal_01_Stage0_Resource];
-                    Entity node = entityManager.Instantiate(nodePrefab);
+                    Entity node = VWorld.Server.GetExistingSystem<PrefabCollectionSystem>()._PrefabGuidToEntityMap[VCreate.Data.Prefabs.TM_Crystal_01_Stage0_Resource];
+                    
+                    
+                    
+                    
                     node.Write<Translation>(new Translation { Value = center });
-                    if (!node.Has<Immortal>())
-                    {
-                        Immortal immortal = new() { IsImmortal = true };
-                        Utilities.AddComponentData(node, immortal);
-                    }
-                    else
-                    {
-                        Immortal immortal = node.Read<Immortal>();
-                        immortal.IsImmortal = true;
-                        node.Write(immortal);
-                    }
-                    Health health = node.Read<Health>();
-                    health.MaxHealth._Value = 50000f;
-                    health.Value = 50000f;
-                    node.Write(health);
-                    Plugin.Logger.LogInfo("Created node, set health...");
+                    
+                    
+                    
                     if (!node.Has<AttachMapIconsToEntity>())
                     {
                         var buffer = entityManager.AddBuffer<AttachMapIconsToEntity>(node);
@@ -119,18 +108,15 @@ namespace VPlus.Hooks
                         var found = node.ReadBuffer<AttachMapIconsToEntity>();
                         found.Add(new AttachMapIconsToEntity { Prefab = VCreate.Data.Prefabs.MapIcon_Siege_Summon_T02_Complete });
                     }
-
-                    infinite = node;
-                    Entity stormPrefab = VWorld.Server.GetExistingSystem<PrefabCollectionSystem>()._PrefabGuidToEntityMap[VCreate.Data.Prefabs.LightningStormType_TeslaHills];
+                    Entity nodeEntity  = entityManager.Instantiate(node);
+                    Plugin.Logger.LogInfo("Created and set node...");
+                    infinite = nodeEntity;
                     Entity zonePrefab = VWorld.Server.GetExistingSystem<PrefabCollectionSystem>()._PrefabGuidToEntityMap[VCreate.Data.Prefabs.TM_Holy_Zone_Area_T02];
                     Entity holy = entityManager.Instantiate(zonePrefab);
-                    Entity lightning = entityManager.Instantiate(stormPrefab);
                     zone = holy;
-                    storm = lightning;
                     holy.Write(new Translation { Value = center });
-                    lightning.Write(new Translation { Value = center });
-                    string red = VPlus.Core.Toolbox.FontColors.Red("Warning:");
-                    string message = $"{red} intense radiation detected at the colosseum. You have a bad feeling about this...";
+                    string red = VPlus.Core.Toolbox.FontColors.Red("Warning");
+                    string message = $"{red}: intense holy radiation detected at the colosseum. The Church must be hiding something!";
                     ServerChatUtils.SendSystemMessageToAllClients(ecb, message);
                 }
                 catch (Exception e)
@@ -151,18 +137,19 @@ namespace VPlus.Hooks
                         if (zone.Has<RadialDamageDebuff>())
                         {
                             RadialDamageDebuff debuff = zone.Read<RadialDamageDebuff>();
-                            debuff.InnerDamagePerSecond *= 1.2f;
-                            if (otherTimer > 3)
-                            {
-                                debuff.OuterDamagePerSecond *= 1.2f;
-                            }
-                        }
-                        else if (storm.Has<RadialDamageDebuff>())
-                        {
-                            RadialDamageDebuff debuff = storm.Read<RadialDamageDebuff>();
-                            debuff.DamageIncrements *= 1.1f;
+                            debuff.InnerDamagePerSecond += 15f;
+                            debuff.OuterDamagePerSecond += 15f;
+                            debuff.InnerRadius += 5f;
+                            debuff.OuterRadius += 5f;
 
+                            
+                            zone.Write(debuff);
                         }
+                        else
+                        {
+                            Plugin.Logger.LogInfo("No radial damage debuff...");
+                        }
+                        
                     }
                     catch (Exception e)
                     {
@@ -171,20 +158,20 @@ namespace VPlus.Hooks
                     switch (otherTimer)
                     {
                         case 1:
-                            string message1 = $"The infinite node is becoming dangerously unstable...";
+                            string message1 = $"The sacred node is becoming unstable. This won't go unnoticed... ";
                             ServerChatUtils.SendSystemMessageToAllClients(ecb, message1);
                             break;
                         case 2:
-                            string message2 = $"Dunley's milita has sent a dispatch to Brighthaven requesting aid.";
+                            string message2 = $"Dunley's militia has sent a dispatch requesting aid from Brighthaven.";
                             ServerChatUtils.SendSystemMessageToAllClients(ecb, message2);
                             break;
                         case 3:
-                            string message3 = $"The Church of Luminance has alerted Solarus, holy nuke inbound...";
+                            string message3 = $"The Church of Luminance is amplifying the holy radiation to purge the area!";
                             ServerChatUtils.SendSystemMessageToAllClients(ecb, message3);
                             break;
                         case 4:
-                            CastNuke();
-                            string message4 = $"The area has been purged by the light. No anomalies remain.";
+                            CleanUp();
+                            string message4 = $"The area has been completely purged by the light. No anomalies remain.";
                             ServerChatUtils.SendSystemMessageToAllClients(ecb, message4);
                             timer = 0;
                             isRunning = false;
@@ -195,54 +182,28 @@ namespace VPlus.Hooks
             }
         }
 
-        public static void CastNuke()
+        public static void CleanUp()
         {
             EntityManager entityManager = VWorld.Server.EntityManager;
-            Entity paladin = VWorld.Server.GetExistingSystem<PrefabCollectionSystem>()._PrefabGuidToEntityMap[VCreate.Data.Prefabs.CHAR_ChurchOfLight_Paladin_VBlood];
-            NetworkId networkId;
-            if (paladin.Has<NetworkId>())
-            {
-                networkId = paladin.Read<NetworkId>();
-            }
-            else
-            {
-                networkId = new NetworkId { Generation = 0, Index = 0 };
-            }
-            
-            Nullable_Unboxed<float3> position = new Nullable_Unboxed<float3>(new float3(-1001, 0, -514));
-            CastAbilityServerDebugEvent castAbilityServerDebugEvent = new CastAbilityServerDebugEvent
-            {
-                AbilityGroup = VCreate.Data.Prefabs.AB_Debug_NukeAll_Group,
-                AimPosition = position,
-                Who = networkId,
-            };
-            IEnumerable<UserModel> online = VRising.GameData.GameData.Users.Online;
-            Entity user;
-            if (!online.Any())
-            {
-                user = new Entity { Index = networkId.Index, Version = networkId.Generation };
-            }
-            else
-            {
-                user = online.First().Entity;
-            }
-            FromCharacter FromCharacter = new() { Character = paladin, User = user };
-            DebugEventsSystem debugEventsSystem = VWorld.Server.GetExistingSystem<DebugEventsSystem>();
-            debugEventsSystem.CastAbilityServerDebugEvent(networkId.Index, ref castAbilityServerDebugEvent, ref FromCharacter);
-            // try to destroy the entities here incase nuke doesnt work or something
             if (entityManager.Exists(zone))
             {
+
                 SystemPatchUtil.Destroy(zone);
             }
-            else if (entityManager.Exists(storm))
+            else
             {
-                SystemPatchUtil.Destroy(storm);
+                Plugin.Logger.LogInfo("Failed to destroy zone.");
             }
-            else if (entityManager.Exists(infinite))
+            
+            if (entityManager.Exists(infinite))
             {
                 SystemPatchUtil.Destroy(infinite);
             }
-            Plugin.Logger.LogInfo("CastNuke complete.");
+            else
+            {
+                Plugin.Logger.LogInfo("Failed to destroy node.");
+            }
+            
         }
     }
 }
