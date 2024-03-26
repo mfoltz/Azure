c1dbc49560167d4a30afc2fc056f8051cebc5cb9
zfolmt <mitchell.foltz@gmail.com>
2024-03-25 21:14:36 -0500

    Update PlayerService.cs

--- Core/Services/PlayerService.cs
+++ Core/Services/PlayerService.cs
@@ -1,10 +1,3 @@
-﻿#region Assembly AdminCommands, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-
-// C:\Users\mitch\Downloads\AdminCommands.dll
-// Decompiled with ICSharpCode.Decompiler 8.1.1.7464
-
-#endregion
-
 using ProjectM.Network;
 using Unity.Collections;
 using Unity.Entities;
